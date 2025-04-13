e39c4337131c9147671f09385cbdec4aa8d1f714
thelightway <thelightway@daum.net>
2025-04-13 22:35:00 +0900

    Init: 초기 프로젝트 설정

---
+++ src/main/java/com/likelion/backendplus4/yakplus/YakplusApplication.java
@@ -0,0 +1,12 @@
+package com.likelion.backendplus4.yakplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakplusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YakplusApplication.class, args);
+
+    }
+}
---
+++ src/test/java/com/likelion/backendplus4/yakplus/YakplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.backendplus4.yakplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
