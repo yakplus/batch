a6d02eff3e508c5d53812a0daf71507ab5ef8c34
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-04-24 12:30:05 +0900

    ✨  Feat: Openai Embedding 함수 추가

--- src/main/java/com/likelion/backendplus4/yakplus/drug/domain/model/port/out/EmbeddingPort.java
+++ src/main/java/com/likelion/backendplus4/yakplus/drug/domain/model/port/out/EmbeddingPort.java
@@ -1,2 +1,5 @@
-package com.likelion.backendplus4.yakplus.drug.domain.model.port.out;public interface EmbeddingPort {
+package com.likelion.backendplus4.yakplus.drug.domain.model.port.out;
+
+public interface EmbeddingPort {
+	float[] getEmbedding(String text); //문자 embedding 하여 float 배열로 반환
 }
--- src/main/java/com/likelion/backendplus4/yakplus/drug/infrastructure/adapter/embedding/OpenaiEmbeddingAdapter.java
+++ src/main/java/com/likelion/backendplus4/yakplus/drug/infrastructure/adapter/embedding/OpenaiEmbeddingAdapter.java
@@ -1,2 +1,40 @@
-package com.likelion.backendplus4.yakplus.drug.infrastructure.adapter.embedding;public class OpenaiEmbeddingAdapter {
+package com.likelion.backendplus4.yakplus.drug.infrastructure.adapter.embedding;
+
+import java.util.List;
+
+import org.springframework.ai.document.MetadataMode;
+import org.springframework.ai.embedding.Embedding;
+import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.OpenAiEmbeddingOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.retry.RetryUtils;
+import org.springframework.stereotype.Service;
+
+import com.likelion.backendplus4.yakplus.drug.domain.model.port.out.EmbeddingPort;
+
+@Service
+public class OpenaiEmbeddingAdapter implements EmbeddingPort {
+
+	private final OpenAiApi openAiApi;
+
+	public OpenaiEmbeddingAdapter(OpenAiApi openAiApi) {
+		this.openAiApi = openAiApi;
+	}
+	@Override
+	public float[] getEmbedding(String text) {
+		OpenAiEmbeddingModel openAiEmbeddingModel = new OpenAiEmbeddingModel(
+			this.openAiApi,
+			MetadataMode.EMBED,
+			OpenAiEmbeddingOptions.builder()
+				.model("text-embedding-ada-002")
+				.build(),
+			RetryUtils.DEFAULT_RETRY_TEMPLATE);
+
+		EmbeddingResponse embeddingResponse = openAiEmbeddingModel
+			.embedForResponse(List.of(text));
+
+		Embedding embedding = embeddingResponse.getResults().getFirst();
+		return embedding.getOutput();
+	}
 }
