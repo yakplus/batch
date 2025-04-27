0b932a2c792a5c2a172158adc1664b1b04c9f409
leelise <jaszzzzz@naver.com>
2025-04-27 20:11:44 +0900

    ♻️ Refactor: EsIndexName을 GovDrugRawDataPort에서 받아오도록 수정하였습니다.

--- src/main/java/com/likelion/backendplus4/yakplus/index/application/service/DrugIndexer.java
+++ src/main/java/com/likelion/backendplus4/yakplus/index/application/service/DrugIndexer.java
@@ -18,14 +18,15 @@ import java.util.List;
  * 약품 색인(인덱싱) 작업을 수행하는 서비스 구현체
  *
  * @since 2025-04-22
- * @modified 2025-04-24
+ * @modified 2025-04-27
+ * 25.04.27 - esIndexname을 인자로 받아 saveAll 메서드에 전달하도록 수정
  */
 @Service
 @RequiredArgsConstructor
 public class DrugIndexer implements IndexUseCase {
     private final GovDrugRawDataPort govDrugRawDataPort;
     private final DrugIndexRepositoryPort drugIndexRepositoryPort;
-    private static final String SORT_BY_PROPERTY = "itemSeq";
+    private static final String SORT_BY_PROPERTY = "drugId";
 
     /**
      * 요청으로 전달된 lastSeq, limit 정보를 바탕으로 RDB에서 데이터를 조회하고
@@ -34,13 +35,29 @@ public class DrugIndexer implements IndexUseCase {
      * @param request 색인 기준 및 개수 정보
      * @author 정안식
      * @since 2025-04-22
-     * @modified 2025-04-24
+     * @modified 2025-04-27
+     * 25.04.27 - esIndexname을 인자로 받아 saveAll 메서드에 전달하도록 수정
      */
     @Override
     public void index(IndexRequest request) {
         Pageable pageable = createPageable(request.limit());
-        List<Drug> drugs = fetchAndTransformRawData(request, pageable);
-        saveDrugs(drugs);
+        List<Drug> drugs = fetchRawData(request, pageable);
+        String esIndexName = getEsIndexName();
+        saveDrugs(esIndexName, drugs);
+    }
+
+    /**
+     * limit 크기 및 DrugId 오름차순 정렬 기준의 객체를 생성한다.
+     *
+     * @param limit 조회할 최대 건수
+     * @return 페이징 객체
+     * @author 정안식
+     * @since 2025-04-22
+     * @modified 2025-04-27
+     * 25.04.27 - itemSeq -> drugId로 수정
+     */
+    private Pageable createPageable(int limit) {
+        return PageRequest.of(0, limit, Sort.by(SORT_BY_PROPERTY).ascending());
     }
 
     /**
@@ -53,21 +70,19 @@ public class DrugIndexer implements IndexUseCase {
      * @since 2025-04-22
      * @modified 2025-04-24
      */
-    private List<Drug> fetchAndTransformRawData(IndexRequest request, Pageable pageable) {
-        return govDrugRawDataPort.fetchRawData(request.lastSeq(), pageable);
+    private List<Drug> fetchRawData(IndexRequest request, Pageable pageable) {
+        return govDrugRawDataPort.fetchRawData(request.lastDrugId(), pageable);
     }
 
     /**
-     * limit 크기 및 itemSeq 오름차순 정렬 기준의 객체를 생성한다.
+     * Elasticsearch 인덱스 이름을 조회한다.
      *
-     * @param limit 조회할 최대 건수
-     * @return 페이징 객체
+     * @return Elasticsearch 인덱스 이름
      * @author 정안식
-     * @since 2025-04-22
-     * @modified 2025-04-24
+     * @since 2025-04-27
      */
-    private Pageable createPageable(int limit) {
-        return PageRequest.of(0, limit, Sort.by(SORT_BY_PROPERTY).ascending());
+    private String getEsIndexName() {
+        return govDrugRawDataPort.getEsIndexName();
     }
 
     /**
@@ -76,10 +91,10 @@ public class DrugIndexer implements IndexUseCase {
      * @param drugs 저장할 도메인 모델 리스트
      * @author 정안식
      * @since 2025-04-22
-     * @modified 2025-04-24
+     * @modified 2025-04-27
+     * 25.04.27 - esIndexName을 인자로 받도록 수정
      */
-    private void saveDrugs(List<Drug> drugs) {
-        //TODO: index 이름 추가 필요
-        drugIndexRepositoryPort.saveAll("indexName", drugs);
+    private void saveDrugs(String esIndexName, List<Drug> drugs) {
+        drugIndexRepositoryPort.saveAll(esIndexName, drugs);
     }
 }
