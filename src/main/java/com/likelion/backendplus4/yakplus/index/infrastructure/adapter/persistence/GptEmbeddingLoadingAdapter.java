13e697d49d2045192f2509039a2fc3e96d1f9a8f
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-04-29 16:19:25 +0900

    ✨  Feat: gptEmbeddingLoadingAdapter 로깅 추가

--- src/main/java/com/likelion/backendplus4/yakplus/index/infrastructure/adapter/persistence/GptEmbeddingLoadingAdapter.java
+++ src/main/java/com/likelion/backendplus4/yakplus/index/infrastructure/adapter/persistence/GptEmbeddingLoadingAdapter.java
@@ -16,6 +16,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.likelion.backendplus4.yakplus.common.util.log.LogUtil.log;
+
 @Repository("gptAdapter")
 @Primary
 @RequiredArgsConstructor
@@ -49,20 +51,29 @@ public class GptEmbeddingLoadingAdapter implements EmbeddingLoadingPort {
     public List<Drug> loadEmbeddingsByPage(Pageable pageable) {
         List<DrugRawDataEntity> rawDataEntities = govDrugJpaRepository.findAll(pageable).getContent();
         List<DrugGptEmbedEntity> drugGptEmbedEntities = govDrugGptEmbedJpaRepository.findAll(pageable).getContent();
+        log("loadEmbeddingsByPage - " + pageable.getPageNumber() +"페이지 에서 받아온 drug 상세정보 수: " + rawDataEntities.size());
+        log("loadEmbeddingsByPage - " + pageable.getPageNumber() +"페이지 에서 받아온 drug 임베딩 벡터수: " + drugGptEmbedEntities.size());
 
         // drugGptEmbedEntities를 Map으로 변환 (key: drugId)
         Map<Long, DrugGptEmbedEntity> gptEmbedMap = new HashMap<>();
         for (DrugGptEmbedEntity embed : drugGptEmbedEntities) {
+            log("loadEmbeddingsByPage - " +pageable.getPageNumber()+"페이지에서 임베딩 벡터 받아온 약품 ID: " + embed.getDrugId());
             gptEmbedMap.put(embed.getDrugId(), embed);
         }
 
         List<Drug> drugs = new ArrayList<>();
+        log("loadEmbeddingsByPage - Drug 도메인 객체 생성 시작");
         for (DrugRawDataEntity drugRawData : rawDataEntities) {
             DrugGptEmbedEntity embed = gptEmbedMap.get(drugRawData.getDrugId());
+            if(embed == null) {
+                log("loadEmbeddingsByPage - " + "Drug 도메인 객체 생성 대상 " + drugRawData.getDrugId() + "의 벡터가 없으므로 skip");
+                continue;
+            }
+            log("loadEmbeddingsByPage - Drug 도메인 객체 생성 대상 " + drugRawData.getDrugId() + "의 벡터 길이: " + embed.getGptVector().length());
             Drug drug = toDomainFromEntity(drugRawData, embed);
             drugs.add(drug);
         }
-
+        log("loadEmbeddingsByPage - Drug 도메인 객체 생성 완료");
         return drugs;
     }
 
